91bc47c8192d8cc11ddccd35768581b8483190bd
Grzegorz.Pandyra <gpandyra@gmail.com>
2025-06-14 00:06:15 +0200

    Clangd

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,16 +9,9 @@ return {
 		 "williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls" }
+				ensure_installed = { "lua_ls", "clangd"}
 			})
 		end
-	},
-	{
-	   "neovim/nvim-lspconfig",
-	   config = function()
-			 local lspconfig = require("lspconfig")
-			 lspconfig.lua_ls.setup({})
-	   end
 	},
 	{
 		"neovim/nvim-lspconfig",
